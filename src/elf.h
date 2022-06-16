114b07b4e57c9a1cdd82a53f54284a0c51be4276
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-06-16 17:06:23 +0530

    add `Elf64_Shdr` struct and define types of sections

--- src/elf.h
+++ src/elf.h
@@ -70,3 +70,97 @@ typedef struct {
 } Elf64_Phdr;
 
 const uint64_t phdr_size = sizeof(Elf64_Phdr);
+
+// This value marks the section header as inactive.
+#define SHT_NULL          0x0
+
+// This section holds information defined by the
+// program, whose format and meaning are determined
+// solely by the program.
+#define SHT_PROGBITS      0x1
+
+// This section holds a symbol table.
+#define SHT_SYMTAB        0x2
+
+// This section holds a string table.  An object file
+// may have multiple string table sections.
+#define SHT_STRTAB        0x3
+
+// This section holds relocation entries with explicit addends.
+#define SHT_RELA          0x4
+
+// This section holds a symbol hash table.
+#define SHT_HASH          0x5
+
+// This section holds information for dynamic linking.
+// An object file may have only one dynamic section.
+#define SHT_DYNAMIC       0x6
+
+// This section holds notes (Elf64_Nhdr).
+#define SHT_NOTE          0x7
+
+// A section of this type occupies no space in the
+// file but otherwise resembles SHT_PROGBITS.
+// Although this section contains no bytes, the
+// sh_offset member contains the conceptual file
+// offset.
+#define SHT_NOBITS        0x8
+
+// This section holds relocation offsets without explicit addends.
+#define SHT_REL           0x9
+
+// This section is reserved but has unspecified semantics.
+#define SHT_SHLIB         0x0A
+
+// This section holds a minimal set of dynamic linking symbols.
+#define SHT_DYNSYM        0x0B
+
+// This section contains an array of pointers to initialization functions.
+#define SHT_INIT_ARRAY    0x0E
+
+// This section contains an array of pointers to termination functions.
+#define SHT_FINI_ARRAY    0x0F
+
+// This section contains an array of pointers to functions
+// that are invoked before all other initialization functions.
+#define SHT_PREINIT_ARRAY 0x10
+
+// This section defines a section group.
+#define SHT_GROUP         0x11
+
+// This section is associated with a section of type SHT_SYMTAB
+// and is required if any of the section header indexes
+// referenced by that symbol table contain the escape value SHN_XINDEX.
+#define SHT_SYMTAB_SHNDX  0x12
+
+// Values in this inclusive range are reserved for operating system-specific semantics.
+#define SHT_LOOS          0x60000000
+#define SHT_HIOS          0x6fffffff
+
+// Values in this inclusive range are reserved for processor-specific semantics.
+#define SHT_LOPROC        0x70000000
+#define SHT_HIPROC        0x7fffffff
+
+// This value specifies the lower bound of the range of indexes
+// reserved for application programs.
+#define SHT_LOUSER        0x80000000
+
+// This value specifies the upper bound of the range of indexes
+// reserved for application programs.
+#define SHT_HIUSER        0xffffffff
+
+
+typedef struct {
+    uint32_t   sh_name;
+    uint32_t   sh_type;
+    uint64_t   sh_flags;
+    uint64_t   sh_addr;
+    uint64_t   sh_offset;
+    uint64_t   sh_size;
+    uint32_t   sh_link;
+    uint32_t   sh_info;
+    uint64_t   sh_addralign;
+    uint64_t   sh_entsize;
+} Elf64_Shdr;
+
+const uint64_t shdr_size = sizeof(Elf64_Shdr);
